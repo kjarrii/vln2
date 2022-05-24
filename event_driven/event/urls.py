795ecc967f6a5dc75174d5b5e06b17762a045c63
Kjartan Freyr Hafþórsson <spalli@spalli.lan>
2022-05-24 15:05:26 +0000

    fylgja tutorial

--- event_driven/event/urls.py
+++ event_driven/event/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+import menu
 
 urlpatterns = [
     path('', views.index, name='event-index'),
+    path('<int:id>', menu.views.getEventById, name="event-details")
 ]
