712e0a8d61b4bca7e1700deccee5af6df98dd7b3
andrik19 <andrik19@ru.is>
2022-05-24 09:53:52 +0000

    url og view

--- event_driven/event/views.py
+++ event_driven/event/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def event(request):
-    return HttpResponse('Hello event')
+def index(request):
+    return render(request, 'event/index.html')
