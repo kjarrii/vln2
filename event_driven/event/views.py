d3b827160ef3e263607bcc8c39b86833c6e82d47
andrik19 <andrik19@ru.is>
2022-05-24 11:38:53 +0000

    fix

--- event_driven/event/views.py
+++ event_driven/event/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+# Create your views here
+
+
 def index(request):
     return render(request, 'event/index.html')
--- event_driven/event_driven/settings.py
+++ event_driven/event_driven/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booking.apps.AppConfig',
+    'booking.apps.BookingConfig',
     'event.apps.EventConfig',
     'menu.apps.MenuConfig',
     'user.apps.UserConfig'
