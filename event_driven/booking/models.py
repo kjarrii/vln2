192acc78ee1438f8f6fbd883b3e7c30f9860253d
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-05-24 11:55:27 +0000

    asdf

--- event_driven/booking/models.py
+++ event_driven/booking/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from user.models import Users
+from event.models import Event
 # Create your models here.
+class Ticket(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    type = models.CharField(max_length=255)
+    price = models.FloatField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+class Bookings(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    bookerid = models.ForeignKey(Users, on_delete=models.CASCADE)
+    tickets = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+
--- event_driven/event/models.py
+++ event_driven/event/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=9999)
+    long = models.IntegerField(blank=False)
+    lat = models.IntegerField(blank=False)
+    address = models.CharField(max_length=255)
+    start = models.DateTimeField(auto_now=False, auto_now_add=False)
+    maximum_attendees = models.PositiveIntegerField(blank=True)
+    keywords = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+
