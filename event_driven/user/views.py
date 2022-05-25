9ec24759def4b5239fd7f3b4d5d50979b902a9a2
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-05-25 10:41:34 +0000

    asdf

--- event_driven/user/views.py
+++ event_driven/user/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return render(request, 'user/index.html')
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    return render(request, 'user/register.html', {
+        'form': UserCreationForm()
+    })
