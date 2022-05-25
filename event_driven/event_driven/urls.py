865a2c18ab11b1e8a194a6bd6702c9e49554dacc
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-05-25 16:38:40 +0000

    asdf

--- event_driven/event_driven/urls.py
+++ event_driven/event_driven/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', include('menu.urls')),
     path('admin/', admin.site.urls),
@@ -24,4 +25,4 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('event/', include('event.urls'))
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
