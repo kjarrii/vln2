22411a27cb708c22d035a42785f9035c70c48a61
andrik19 <andrik19@ru.is>
2022-05-25 12:08:23 +0000

    loginpage

--- event_driven/user/urls.py
+++ event_driven/user/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('register', views.register, name='register'),
-    path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('login', LoginView.as_view(template_name='user/login-reg-base.html'), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout')
 ]
