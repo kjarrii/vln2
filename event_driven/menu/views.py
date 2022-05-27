f241596213c9cd85b609f07cc8552f1c33d793c2
andrik19 <andrik19@ru.is>
2022-05-27 19:39:20 +0000

    menu

--- event_driven/menu/views.py
+++ event_driven/menu/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-
+from event.models import Event
 
 # Create your views here.
 def index(request):
-    return render(request, 'menu/index.html', context={ 'pro':pro })
-
-def getEventById(request, id):
-    return render(request, 'event.html', {
-        'event': get_object_or_404(event, pk=id)
-    })
+    context = {'events': Event.objects.all().order_by('id')}
+    return render(request, 'menu/index.html', context)
 
 
-pro = [
-    {'title': 'Sturla Atlas in Laugardalshöll', 'date': '01.12.2022', 'location': 'laugardalshöll', 'picture': '../../static/images/sturla1.jpeg'},
-    {'title': 'Kaleó in Hlaðan', 'date': '01.12.2022', 'location': 'Hlaðan, gufunesbær', 'picture': '../../static/images/sturla1.jpeg'},
-    {'title': 'Leikir - Stjarnan', 'date': '01.12.2022', 'location': 'TM-höllin', 'picture': '../../static/images/sturla1.jpeg'},
-    {'title': 'dek', 'date': '01.12.2022', 'location': 'skeggi', 'picture': '../../static/images/sturla1.jpeg'},
-]
