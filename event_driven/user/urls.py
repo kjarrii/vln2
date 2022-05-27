450a4b616333cfbea2db4f80116316394703357d
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-05-27 13:32:15 +0000

    asdf

--- event_driven/user/urls.py
+++ event_driven/user/urls.py
@@ -6,10 +6,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    #path('register', views.register, name='register'),
     path('profile', views.profile, name='profile'),
-    #path('login', LoginView.as_view(template_name='user/login-reg-base.html'), name='login'),
-    #path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register')
