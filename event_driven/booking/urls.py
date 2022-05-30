750c256e5b49527cd505c563f160238abbd7b5fc
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-05-30 14:21:39 +0000

    booking 1

--- event_driven/booking/urls.py
+++ event_driven/booking/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='booking-index'),
+    path('<int:id>', views.index, name='booking-index'),
 ]
--- event_driven/booking/views.py
+++ event_driven/booking/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from event.models import Event
 
 # Create your views here.
-def index(request):
-    return render(request, 'booking/index.html')
+def index(request,id):
+    current_event = Event.objects.get(pk=id)
+    context = {'event': current_event}
+    return render(request, 'booking/select_tickets.html', context)
