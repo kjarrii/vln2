d80aef45137bc2831e25caf173036fe451281e9f
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-05-30 12:44:08 +0000

    similar events

--- event_driven/event/views.py
+++ event_driven/event/views.py
@@ -2,15 +2,38 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from event.models import Event
 from user.forms.forms import EventCreateForm
+import datetime
+import pytz
 
 def index(request):
     context = {'events': Event.objects.all().order_by('id')}
     return render(request, 'event/index.html', context)
 
 def get_event_by_id(request, id):
-    return render(request, 'event/index.html', {
-        'event': get_object_or_404(Event, pk=id)
-    })
+    org_events = Event.objects.all().order_by('name')
+    current_event = Event.objects.get(pk=id)
+    current_event_keywords = current_event.keywords
+    current_event_keywords_list = current_event_keywords.split(',')
+    keyword_events = []
+    for event in org_events:
+        temp_list_of_keywords = event.keywords.split(',')
+        if event.id == id:
+            continue
+        else:
+            for key in current_event_keywords_list:
+                if key in temp_list_of_keywords:
+                    keyword_events.append(event)
+                    break
+    similar_events = []
+    utc = pytz.UTC
+    curr_date = utc.localize(datetime.datetime.now())
+    for event in keyword_events:
+        start_date = event.start
+        if start_date > curr_date:
+            similar_events.append(event)
+
+    context = {'event': get_object_or_404(Event, pk=id), 'similar_events': similar_events}
+    return render(request, 'event/index.html', context)
 
 def create_event(request):
     if request.method == 'POST':
