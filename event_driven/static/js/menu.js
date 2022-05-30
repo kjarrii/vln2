122e01eebad8797300327b465b62f357878419a2
andrik19 <andrik19@ru.is>
2022-05-30 12:07:23 +0000

    menu

--- event_driven/static/js/menu.js
+++ event_driven/static/js/menu.js
@@ -4,10 +4,4 @@ function displayDropdown() {
     dropdown.classList.toggle('invisible')
 }
 
-/*document.addEventListener('click', function(event) {
-    console.log(event)
-    if ((event.path[0].className !== 'navbar-option') && event.path[0].className !== 'profile-image-small')) {
-        dropdown.classList.toggle('invisible')
-    }
-})*/
 
