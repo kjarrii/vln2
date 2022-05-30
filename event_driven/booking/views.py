f4baa1aefaf3abfb063896725775818dad578239
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-05-30 14:34:22 +0000

    booking 2

--- event_driven/booking/views.py
+++ event_driven/booking/views.py
@@ -4,6 +4,10 @@ from event.models import Event
 
 # Create your views here.
 def index(request,id):
-    current_event = Event.objects.get(pk=id)
-    context = {'event': current_event}
-    return render(request, 'booking/select_tickets.html', context)
+    if request.user.is_authenticated:
+        current_event = Event.objects.get(pk=id)
+        context = {'event': current_event}
+        return render(request, 'booking/select_tickets.html', context)
+    else:
+        return render(request, 'user/login.html')
+
