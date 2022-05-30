53b89190f1843ac6fa4b621de69ca412001054a8
andrik19 <andrik19@ru.is>
2022-05-30 11:11:27 +0000

    frick

--- event_driven/static/js/menu.js
+++ event_driven/static/js/menu.js
@@ -1,17 +1,13 @@
 let dropdown = document.getElementById('dropdown-visibility')
 
 function displayDropdown() {
-    if (dropdown.classList.contains('invisible')) {
-        dropdown.classList.remove('invisible')
-    }
-    else {
-        dropdown.classList.add('invisible')
-    }
+    dropdown.classList.toggle('invisible')
 }
 
-document.addEventListener('click', function(event) {
+/*document.addEventListener('click', function(event) {
     console.log(event)
-    if (!dropdown.classList.contains('invisible') && (event.path[0].className !== 'navbar-option') && event.path[0].className !== 'profile-image-small') {
-        dropdown.classList.add('invisible')
+    if ((event.path[0].className !== 'navbar-option') && event.path[0].className !== 'profile-image-small')) {
+        dropdown.classList.toggle('invisible')
     }
-})
+})*/
+
