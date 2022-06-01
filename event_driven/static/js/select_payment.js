3306b9cdddae57ec1de93b00c4927a7e3f6d47a3
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-06-01 13:45:44 +0000

    asdf

--- event_driven/static/js/select_payment.js
+++ event_driven/static/js/select_payment.js
@@ -1,26 +1,20 @@
-console.log("selecting payment")
-ticket_element = document.getElementById('id_tickets')
-name_element = document.getElementById('id_name')
-email_element = document.getElementById('id_email')
-phone_element = document.getElementById('id_phone')
-full_name_element = document.getElementById('id_full_name')
-street_element = document.getElementById('id_street')
-no_element = document.getElementById('id_no')
-city_element = document.getElementById('id_city')
-zip_element = document.getElementById('id_zip')
-country_element = document.getElementById('id_country')
-event_element = document.getElementById('id_eventid')
-user_element = document.getElementById('id_userid')
-delivery_element = document.getElementById('id_delivery_method')
-submit_button = document.getElementById('submit_button')
-console.log(ticket_element)
+ticket_element = document.getElementById('id_tickets');name_element = document.getElementById('id_name');email_element = document.getElementById('id_email');phone_element = document.getElementById('id_phone');full_name_element = document.getElementById('id_full_name');street_element = document.getElementById('id_street');no_element = document.getElementById('id_no');city_element = document.getElementById('id_city');zip_element = document.getElementById('id_zip');country_element = document.getElementById('id_country');event_element = document.getElementById('id_eventid');user_element = document.getElementById('id_userid');delivery_element = document.getElementById('id_delivery_method');submit_button = document.getElementById('submit_button');
 
+let name_on_card_element = document.getElementById('name_on_card_input');
+let card_number_element = document.getElementById('card_number_input');
+let expiration_date_element = document.getElementById('expiration_date_input');
+let cvc_element = document.getElementById('cvc_input');
 function go_back () {
     window.location.href = 'select_delivery'
 }
 
 function verify_input(total_tickets) {
-    return true
+    if (name_on_card_element.value !== '' && card_number_element.value !== '' && expiration_date_element.value !== '' && cvc_element.value !== ''){
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 function delete_this_function() {
@@ -37,21 +31,7 @@ function delete_this_function() {
     sessionStorage.setItem('delivery_method', 'postal_service');
 }
 function go_forward (){
-    delete_this_function()
-    ticket_element.value = sessionStorage.getItem('tickets')
-    name_element.value = sessionStorage.getItem('name')
-    email_element.value = sessionStorage.getItem('email')
-    phone_element.value = sessionStorage.getItem('phone')
-    full_name_element.value = sessionStorage.getItem('full_name')
-    street_element.value = sessionStorage.getItem('street')
-    no_element.value = sessionStorage.getItem('no')
-    city_element.value = sessionStorage.getItem('city')
-    zip_element.value = sessionStorage.getItem('zip')
-    country_element.value = sessionStorage.getItem('country')
-    event_element.value = sessionStorage.getItem('eventid')
-    delivery_element.value = sessionStorage.getItem('delivery_method')
-    user_element.value = JSON.parse(document.getElementById('user_id').textContent);
-
+    ticket_element.value = sessionStorage.getItem('tickets');name_element.value = sessionStorage.getItem('name');email_element.value = sessionStorage.getItem('email');phone_element.value = sessionStorage.getItem('phone');full_name_element.value = sessionStorage.getItem('full_name');street_element.value = sessionStorage.getItem('street');no_element.value = sessionStorage.getItem('no');city_element.value = sessionStorage.getItem('city');zip_element.value = sessionStorage.getItem('zip');country_element.value = sessionStorage.getItem('country');event_element.value = sessionStorage.getItem('eventid');delivery_element.value = sessionStorage.getItem('delivery_method');user_element.value = JSON.parse(document.getElementById('user_id').textContent);
     if (verify_input()) {
 
         submit_button.click()
