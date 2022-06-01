fd3bdffb20678dd7d8613a6c73f12513f9574b1f
Kjartan Freyr Hafþórsson <spalli@spalli.lan>
2022-06-01 10:15:14 +0000

    lsjd

--- event_driven/static/js/profile.js
+++ event_driven/static/js/profile.js
@@ -1,14 +1,34 @@
-
+let element = document.getElementById('favorite_categories');
+let now_fav = element.getAttribute('nowFav');
+const fav = now_fav.split(",");
+let today = new Date();
+let date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+let time = today.getHours() + ':' + today.getMinutes();
 
 function pop_fav() {
-    let element = document.getElementById('favorite_categories');
-    let now_fav = element.getAttribute('nowFav');
-    const fav = now_fav.split(",");
         for (let i = 0; i < fav.length; i++) {
             let di = document.createElement('div');
-            di.innerHTML = '<div class="fav-cat">'+ fav[i] +'<a class="del-fav-btn" >X</a></div>';
-            element.appendChild(di);
-}}
+            di.innerHTML = '<div class="fav-cat" id="catfav">'+ fav[i] +'<a class="del-fav-btn" del-date="'+date + ' ' + time +'">X</a></div>';
+            di.onclick = deleteItem;
+            element.appendChild(di)
+        }
+
+        function deleteItem(e) {
+            let ul = document.getElementById('catfav');
+            ul.removeChild(di);
+
+            let list = document.getElementById('favorite_categories');
+            list.addEventListener('click', function(e) {
+            let delDate = e.target.getAttribute('del-date');
+            let index = fav.map((item) => item.date).indexOf(delDate);
+            console.log(index);
+            fav.splice(index, 1);
+            console.log(fav)
+    });
+  }
+
+
+  }
 
 function not_fav() {
     let element = document.getElementById('favorite_categories');
