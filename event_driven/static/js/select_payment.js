05a8168b711e1d183ea2e9d374ec164c68174400
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-06-01 12:17:30 +0000

    asdf

--- event_driven/static/js/select_payment.js
+++ event_driven/static/js/select_payment.js
@@ -1,4 +1,18 @@
 console.log("selecting payment")
+ticket_element = document.getElementById('id_tickets')
+name_element = document.getElementById('id_name')
+email_element = document.getElementById('id_email')
+phone_element = document.getElementById('id_phone')
+full_name_element = document.getElementById('id_full_name')
+street_element = document.getElementById('id_street')
+no_element = document.getElementById('id_no')
+city_element = document.getElementById('id_city')
+zip_element = document.getElementById('id_zip')
+country_element = document.getElementById('id_country')
+event_element = document.getElementById('id_eventid')
+user_element = document.getElementById('id_userid')
+submit_button = document.getElementById('submit_button')
+console.log(ticket_element)
 
 function go_back () {
     window.location.href = 'select_delivery'
@@ -14,7 +28,7 @@ function delete_this_function() {
     sessionStorage.setItem('email', 'karigeorgs@gmail.com');
     sessionStorage.setItem('phone', '+6969696996');
     sessionStorage.setItem('full_name', 'Kári Georgsson');
-    sessionStorage.setItem('steet', 'Bakers');
+    sessionStorage.setItem('street', 'Bakers');
     sessionStorage.setItem('no', '21');
     sessionStorage.setItem('city', 'London');
     sessionStorage.setItem('zip', '210');
@@ -22,8 +36,21 @@ function delete_this_function() {
 }
 function go_forward (){
     delete_this_function()
+    ticket_element.value = sessionStorage.getItem('tickets')
+    name_element.value = sessionStorage.getItem('name')
+    email_element.value = sessionStorage.getItem('email')
+    phone_element.value = sessionStorage.getItem('phone')
+    full_name_element.value = sessionStorage.getItem('full_name')
+    street_element.value = sessionStorage.getItem('street')
+    no_element.value = sessionStorage.getItem('no')
+    city_element.value = sessionStorage.getItem('city')
+    zip_element.value = sessionStorage.getItem('zip')
+    country_element.value = sessionStorage.getItem('country')
+    event_element.value = sessionStorage.getItem('eventid')
+    user_element.value = JSON.parse(document.getElementById('user_id').textContent);
+
     if (verify_input()) {
-        console.log(sessionStorage.getItem('tickets'))
-        window.location.href = 'reciept'
+
+        submit_button.click()
     }
 }
