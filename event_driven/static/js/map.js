eb75f4160e18d21341107af013ccb361fc78c8ec
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-06-02 12:07:17 +0000

    sdfg

--- event_driven/static/js/map.js
+++ event_driven/static/js/map.js
@@ -1,21 +1,72 @@
-function initMap() {
-  const myLatLng = { lat: -25.363, lng: 131.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: myLatLng,
-  });
-
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    label: {
-      text: "$300k",
-      color: "#0a0a0a",
-      fontSize: "30px",
-      fontWeight: "bold"
+     var markers = [
+    {
+        "title": 'Aksa Beach',
+        "lat": '19.1759668',
+        "lng": '72.79504659999998',
+        "description": 'Aksa Beach is a popular beach and a vacation spot in Aksa village at Malad, Mumbai.'
     },
-  });
-  Marker.showInfoWindow();
-}
+    {
+        "title": 'Juhu Beach',
+        "lat": '19.0883595',
+        "lng": '72.82652380000002',
+        "description": 'Juhu Beach is one of favourite tourist attractions situated in Mumbai.'
+    },
+    {
+        "title": 'Girgaum Beach',
+        "lat": '18.9542149',
+        "lng": '72.81203529999993',
+        "description": 'Girgaum Beach commonly known as just Chaupati is one of the most famous public beaches in Mumbai.'
+    },
+    {
+        "title": 'Jijamata Udyan',
+        "lat": '18.979006',
+        "lng": '72.83388300000001',
+        "description": 'Jijamata Udyan is situated near Byculla station is famous as Mumbai (Bombay) Zoo.'
+    },
+    {
+        "title": 'Sanjay Gandhi National Park',
+        "lat": '19.2147067',
+        "lng": '72.91062020000004',
+        "description": 'Sanjay Gandhi National Park is a large protected area in the northern part of Mumbai city.'
+    }
+    ];
+    window.onload = function () {
+        LoadMap();
+    }
+    function LoadMap() {
+        var mapOptions = {
+            center: new google.maps.LatLng(markers[0].lat, markers[0].lng),
+            zoom: 10,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        //Create and open InfoWindow.
+        var infoWindow = new google.maps.InfoWindow();
+ 
+        for (var i = 0; i < markers.length; i++) {
+            var data = markers[i];
+            var myLatlng = new google.maps.LatLng(data.lat, data.lng);
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                eventid: 19,
+                label: {
+                  text: "$300k",
+                  color: "#0a0a0a",
+                  fontSize: "15px",
+                  fontWeight: "bold"
+                },
+                title: data.title
+            });
+ 
+            //Attach click event to the marker.
+            (function (marker, data) {
+                google.maps.event.addListener(marker, "click", function (e) {
+                    //Wrap the content inside an HTML DIV in order to set height and width of InfoWindow.
+                      window.location.href = 'event/' + marker.eventid.toString()
 
-window.initMap = initMap;
+                });
+            })(marker, data);
+        }
+    }
