6cec34b83ebd96af0b7bff5de8927261be7bd116
Kjartan Freyr Hafþórsson <spalli@spalli.lan>
2022-06-02 17:48:37 +0000

    lksdjf

--- event_driven/static/js/profile.js
+++ event_driven/static/js/profile.js
@@ -1,53 +1,58 @@
 let element = document.getElementById('favorite_categories');
+let favElements = document.getElementById('fav_cat_elements');
 let now_fav = element.getAttribute('nowFav');
 const fav = now_fav.split(",");
-let today = new Date();
-let date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
-let time = today.getHours() + ':' + today.getMinutes();
+let all_fav = element.getAttribute('allFav');
+const all = all_fav.split(",");
+let f = new Set(fav);
+let notFav = all.filter( x => !f.has(x) );
 
 function pop_fav() {
         for (let i = 0; i < fav.length; i++) {
             let di = document.createElement('div');
-            di.innerHTML = '<div class="fav-cat" id="catfav">'+ fav[i] +'<a class="del-fav-btn" del-date="'+date + ' ' + time +'">X</a></div>';
-            di.onclick = deleteItem;
-            element.appendChild(di)
+            di.innerHTML = '<div class="fav-cat">'+ fav[i] +'<a class="del-fav-btn" del-date="'+i +'">X</a></div>';
+            di.setAttribute('id', i.toString());
+            di.setAttribute('onclick', 'deletecat(this.id)');
+            favElements.appendChild(di)
         }
 
-        function deleteItem(e) {
-            let ul = document.getElementById('catfav');
-            ul.removeChild(di);
-
-            let list = document.getElementById('favorite_categories');
-            list.addEventListener('click', function(e) {
-            let delDate = e.target.getAttribute('del-date');
-            let index = fav.map((item) => item.date).indexOf(delDate);
-            console.log(index);
-            fav.splice(index, 1);
-            console.log(fav)
-    });
+    not_fav();
   }
 
+function deletecat(eleid) {
+    let de = document.getElementById(eleid);
+    fav.splice(eleid,1);
+    favElements.innerHTML = '';
+    pop_fav();
 
-  }
+
+}
 
 function not_fav() {
-    let element = document.getElementById('favorite_categories');
-    let now_fav = element.getAttribute('nowFav');
-    let all_fav = element.getAttribute('allFav');
-    const fav = now_fav.split(",");
-    const all = all_fav.split(",");
-    const f = new Set(fav);
-    const notFav = all.filter( x => !f.has(x) );
-
+    f = new Set(fav);
+    notFav = all.filter( x => !f.has(x) );
     let drp = document.getElementById('fav-dropdwn');
+    drp.innerHTML = '';
+
+    let elo = document.createElement("option");
+        elo.textContent = 'Add category';
+        drp.appendChild(elo);
+
     for(var i = 0; i < notFav.length; i++) {
         var opt = notFav[i];
         var el = document.createElement("option");
         el.textContent = opt;
-        el.value = opt;
+        el.value = i.toString();
+        //el.setAttribute('id', i.toString());
+        drp.setAttribute('onchange', 'addcat(this.value)');
         drp.appendChild(el);
 
 }}
 
-not_fav()
+function addcat(addid){
+    fav.push(notFav[addid]);
+    favElements.innerHTML = '';
+    pop_fav();
+}
+
 pop_fav()
