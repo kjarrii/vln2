70f331729772d9185b64902d2b620ba0725874a3
KariGeorgs <42555469+KariGeorgs@users.noreply.github.com>
2022-06-02 14:46:34 +0000

    asdf

--- event_driven/booking/views.py
+++ event_driven/booking/views.py
@@ -2,13 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from event.models import Event
 from booking.forms.booking_form import BookingForm
+import datetime
+import pytz
 
 # Create your views here.
+def has_passed(start_date):
+    utc = pytz.UTC
+    curr_date = utc.localize(datetime.datetime.now())
+    if start_date < curr_date:
+        return True
+    else:
+        return False
+
 def index(request,id):
     if request.user.is_authenticated:
         current_event = Event.objects.get(pk=id)
-        context = {'event': current_event}
-        return render(request, 'booking/select_tickets.html', context)
+        if current_event.tickets_left() == 0:
+            return HttpResponseRedirect('../user/logout')
+        elif has_passed(current_event.start):
+            return HttpResponseRedirect('../user/logout')
+        else:
+            context = {'event': current_event}
+            return render(request, 'booking/select_tickets.html', context)
     else:
         return render(request, 'user/login.html')
 
