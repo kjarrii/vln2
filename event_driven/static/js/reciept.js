c2ea4dc0d3b6eceee09dd8d5778c7d60a2af191b
Kjartan Freyr Hafþórsson <spalli@spalli.lan>
2022-06-03 12:17:50 +0000

    kjds

--- event_driven/static/js/reciept.js
+++ event_driven/static/js/reciept.js
@@ -1,3 +1,37 @@
 let asdf = sessionStorage.getItem('eventid')
 
+let titElem = document.getElementById("ev-tit");
+let locElem = document.getElementById("ev-tit");
+let totElem = document.getElementById("ev-tit");
+let delElem = document.getElementById("ev-tit");
+let titVal = sessionStorage.getItem('event_name');
+let locVal = sessionStorage.getItem('');
+let totVal = sessionStorage.getItem('');
+let delVal = sessionStorage.getItem('');
+
+console.log(titVal.value);
+
+let tittag = document.createElement('p');
+    let texttit = document.createTextNode(titVal);
+    tittag.appendChild(texttit);
+    titElem.appendChild(tittag);
+
+let loctag = document.createElement('p');
+    let texloc = document.createTextNode(locVal);
+    loctag.appendChild(texloc);
+    locElem.appendChild(loctag);
+
+let tottag = document.createElement('p');
+    let textot = document.createTextNode(totVal);
+    tottag.appendChild(textot);
+    totElem.appendChild(tottag);
+
+let deltag = document.createElement('p');
+    let texdel = document.createTextNode(delVal);
+    deltag.appendChild(texdel);
+    delElem.appendChild(deltag);
+
+
+
+
 sessionStorage.clear() // Þetta á að vera í endan til að hreinsa all bókinarferlið út, á ekki að vera hægt að fara til baka
